07e569bddb33790e6f450a2ce3159d5567632f23
AMRI <mamri7414@gmail.com>
2026-06-28 22:39:29 +0200

    handle anti-bot page in CI

--- features/support/hooks.ts
+++ features/support/hooks.ts
@@ -9,7 +9,14 @@ Before(async function (this: CustomWorld) {
     this.browser = await chromium.launch({
         headless: process.env.CI ? true : false
     });
-    this.context = await this.browser.newContext();
+    //this.context = await this.browser.newContext();
+    this.context = await this.browser.newContext({
+    userAgent:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+    locale: "en-US",
+    viewport: { width: 1280, height: 720 }
+    });
+    
     this.page = await this.context.newPage();
 });
 
